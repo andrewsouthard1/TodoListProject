4d3cb99a12266f1ac82d37e2f96f83b57c22ec03
Andrew Southard <andrewsouthard111@gmail.com>
2016-06-22 19:02:21 -0400

    finished

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,7 +1,26 @@
 //Check off specific todos by clicking
-$("li").click(function(){
+$("ul").on("click", "li", function(){
 	//if li is gray
-	// if
-	// $(this).css(
-	$(this).css("color", "gray");
+	$(this).toggleClass("completed");
+});
+
+//click on X to delete Todos
+$("ul").on("click", "span", function(){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+})
+
+$("input[type='text'").keypress(function(event){
+	if(event.which === 13){
+		var todoText = $(this).val();
+		$(this).val("");
+		//create a new li and add to ul
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span>" + todoText + "</li>");
+	}
+})
+
+$(".fa-plus").click(function(){
+	$("input[type='text'").fadeToggle();
 })
